a8fc620c446696bfb46f6c7545e3d60d5b661483
Brady Bonnette <brady.bonnette@gmail.com>
2011-04-03 22:51:57 -0400

    Adding layouts, images and misc. changes that were originally from the prototype project.

--- src/main/java/com/brbware/shoppinglist/ListItemActivity.java
+++ src/main/java/com/brbware/shoppinglist/ListItemActivity.java
@@ -3,8 +3,9 @@ package com.brbware.shoppinglist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
-public class HelloAndroidActivity extends Activity {
+public class ListItemActivity extends Activity {
 
     private static String TAG = "shoppinglist";
 
@@ -16,9 +17,16 @@ public class HelloAndroidActivity extends Activity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
 		Log.i(TAG, "onCreate");
+
         setContentView(R.layout.main);
+
+        ListView mainListView = (ListView) findViewById(R.id.mainListView);
+
+        
     }
 
 }
